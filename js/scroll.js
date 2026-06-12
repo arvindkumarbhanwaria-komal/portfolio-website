c70ffdeb7aea264a22056d410c7af730f248f757
ARVIND KUMAR BHANWARIA <arvindkumarbhanwaria@gmail.com>
2026-06-12 13:30:48 +0530

    Initial portfolio website upload

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+// ===================================
+// SCROLL.JS
+// ===================================
+
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
